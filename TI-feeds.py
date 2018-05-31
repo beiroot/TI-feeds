c7592f783006f6b12c95cfe7fbd46b60975df070
Maciej Stanislawski <maciej.stanislawski@orlen.pl>
2018-05-31 20:25:07 +0200

    clearing code; separation of declaration and execution code

--- TI-feeds.py
+++ TI-feeds.py
@@ -36,10 +36,10 @@ class Feed:
             # request = requests.get(link + self.feedpath, auth=HTTPBasicAuth(login, passw))
             if not request.ok:
                 logger.critical(f"Could not get data from: {link}")
-                sys.exit()
+                sys.exit("error getting data")
         except (requests.ConnectionError, requests.Timeout):
             logger.critical("Couldn't get the data, quitting")
-            sys.exit()
+            sys.exit("error getting data")
         return request
 
     @staticmethod
@@ -55,7 +55,7 @@ class Feed:
             xmlroot = xmlparse.getroot()
         except PermissionError:
             logger.critical(f"No rights to folder: {output} . Quitting.")
-            sys.exit()
+            sys.exit("Permission error")
         return xmlroot
 
     @staticmethod
@@ -154,65 +154,67 @@ def feed_compare():
     return
 
 
-feed_files = {
-                "contr_addr": "TI-controllers-address.txt",
-                "contr_IP": "TI-controllers-IP.txt",
-                "contr_url": "TI-controllers-url.txt",
-                "contr_csv": "TI-controllers.csv",
-                "url_mal": "TI-URL-malware.txt",
-                "url_phish": "TI-URL-phishing.txt",
-            }
+if __name__ == '__main__':
 
-# REGEX
-re1 = '(.*)'   # Any Character
-re2 = '(.)'	    # Dot
-re3 = '([a-z])'   # Any Single Word Character (Not Whitespace)
+    # REGEX
+    re1 = '(.*)'  # Any Character
+    re2 = '(.)'  # Dot
+    re3 = '([a-z])'  # Any Single Word Character (Not Whitespace)
 
-rg = re.compile(re1+re2+re3, re.IGNORECASE | re.DOTALL)
+    rg = re.compile(re1 + re2 + re3, re.IGNORECASE | re.DOTALL)
 
-# Parse arguments
-argp = argparse.ArgumentParser()
-argp.add_argument("-o", metavar="FOLDER", help="destination folder e.g. /nfs/scripts")
-args = argp.parse_args()
+    feed_files = {
+                    "contr_addr": "TI-controllers-address.txt",
+                    "contr_IP": "TI-controllers-IP.txt",
+                    "contr_url": "TI-controllers-url.txt",
+                    "contr_csv": "TI-controllers.csv",
+                    "url_mal": "TI-URL-malware.txt",
+                    "url_phish": "TI-URL-phishing.txt",
+                }
 
-try:
-    output = args.o + "/output/"
-    archive = args.o + "/archive/" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
-    temp = args.o + "/temp/"
-except TypeError:
-    output = os.getcwd() + "/output/"
-    archive = os.getcwd() + "/archive/" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
-    temp = os.getcwd() + "/temp/"
+    # Parse arguments
+    argp = argparse.ArgumentParser()
+    argp.add_argument("-o", metavar="FOLDER", help="destination folder e.g. /nfs/scripts")
+    args = argp.parse_args()
 
-# Read the old file and compare them with new data
-for value in feed_files.values():
-    feed_compare_add_old(value)
+    try:
+        output = args.o + "/output/"
+        archive = args.o + "/archive/" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
+        temp = args.o + "/temp/"
+    except TypeError:
+        output = os.getcwd() + "/output/"
+        archive = os.getcwd() + "/archive/" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
+        temp = os.getcwd() + "/temp/"
 
-# move old files
-try:
-    logger.debug(f"moving files from {output} to {archive}")
-    shutil.move(output, archive)
-except FileNotFoundError:
-    pass
+    # Read the old file and compare them with new data
+    for value in feed_files.values():
+        feed_compare_add_old(value)
 
-# CONTROLLERS FEED #
-contr = Feed("contr", "controllerszip/controllers.xml.zip")
-rcontr = contr.download()
-xmlroot = contr.unzip(rcontr)
-contr.parse(xmlroot, feed_files["contr_addr"], "botnet", "address")
-contr.parse(xmlroot, feed_files["contr_IP"], "controller", "ip")
-contr.parse(xmlroot, feed_files["contr_url"], "http", "url")
-contr.parse_to_csv(xmlroot, feed_files["contr_csv"], "botnet")
+    # move old files
+    try:
+        logger.debug(f"moving files from {output} to {archive}")
+        shutil.move(output, archive)
+    except FileNotFoundError:
+        pass
 
-# URL MALWARE FEED #
-mal = Feed("malware", "malwareurl.txt")
-rmal = mal.download()
-mal.parse_url(feed_files["url_mal"], rmal)
+    # CONTROLLERS FEED #
+    contr = Feed("contr", "controllerszip/controllers.xml.zip")
+    rcontr = contr.download()
+    xmlroot = contr.unzip(rcontr)
+    contr.parse(xmlroot, feed_files["contr_addr"], "botnet", "address")
+    contr.parse(xmlroot, feed_files["contr_IP"], "controller", "ip")
+    contr.parse(xmlroot, feed_files["contr_url"], "http", "url")
+    contr.parse_to_csv(xmlroot, feed_files["contr_csv"], "botnet")
 
-# URL PHISHING FEED #
-phish = Feed("phishing", "phishing_last.txt")
-rphish = phish.download()
-phish.parse_url(feed_files["url_phish"], rphish)
+    # URL MALWARE FEED #
+    mal = Feed("malware", "malwareurl.txt")
+    rmal = mal.download()
+    mal.parse_url(feed_files["url_mal"], rmal)
 
-# comparing old and new feed files
-feed_compare()
+    # URL PHISHING FEED #
+    phish = Feed("phishing", "phishing_last.txt")
+    rphish = phish.download()
+    phish.parse_url(feed_files["url_phish"], rphish)
+
+    # comparing old and new feed files
+    feed_compare()
